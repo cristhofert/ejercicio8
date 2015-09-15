2438dc66f260826adacfe2bdc626f346656bb6e1
CinthyaC <ciincalbete123@gmail.com>
2015-09-15 19:52:04 -0300

    attr_accessor e initialize

--- actividad.rb
+++ actividad.rb
@@ -1,2 +1,9 @@
+require_relative event.rb
 class Actividad
+attr_accessor :name, :date, :time_start, :time_end, :type
+
+	def initialize()
+	type=["taller", "charla", "mesa de debate"]
+	date=event.date
+	end
 end
